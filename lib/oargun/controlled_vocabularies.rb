0909e4b22f8dce0e0dd898dfed27ea99646b89b0
Justin Coyne <justin@curationexperts.com>
2015-01-12 13:31:57 -0600

    Require qa in the ControlledVocabularies module

--- lib/oargun/controlled_vocabularies.rb
+++ lib/oargun/controlled_vocabularies.rb
@@ -1,3 +1,6 @@
+# Don't include QA when we're testing because we don't have a database setup
+require 'qa' unless defined? WITHOUT_QA
+
 module Oargun::ControlledVocabularies
   extend ActiveSupport::Autoload
   autoload :Geographic
