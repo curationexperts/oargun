ffad5286f793676125687c4d4776adfae1100f8d
val99erie <val99erie@gmail.com>
2015-03-31 13:53:43 -0500

    Added a test and refactored

--- spec/unit/rights_statement_spec.rb
+++ spec/unit/rights_statement_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Oargun::ControlledVocabularies::RightsStatement do
   subject { described_class.new(uri) }
+
   context 'when the rights statement is in the vocabulary' do
     let(:uri) { RDF::URI.new('http://www.europeana.eu/rights/rr-f/') }
 
@@ -11,5 +12,15 @@ describe Oargun::ControlledVocabularies::RightsStatement do
   context 'when the rights statement is not in the vocabulary' do
     let(:uri) { RDF::URI.new('http://foo.bar/rights/rr-f/') }
     it { is_expected.not_to be_valid }
+  end
+
+  describe '#rdf_label' do
+    context 'with an opaquenamespace URI' do
+      let(:uri) { RDF::URI.new('http://opaquenamespace.org/ns/rights/educational/') }
+
+      it 'finds the label' do
+        expect(subject.rdf_label).to eq ['Educational Use Permitted']
+      end
+    end
   end
 end
