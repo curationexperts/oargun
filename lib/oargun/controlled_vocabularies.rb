9371ceb4fdc22d60fb33013240b6db1281e2a6b9
Justin Coyne <justin@curationexperts.com>
2015-04-17 16:20:22 -0500

    Remove QA dependency

--- lib/oargun/controlled_vocabularies.rb
+++ lib/oargun/controlled_vocabularies.rb
@@ -1,6 +1,3 @@
-# Don't include QA when we're testing because we don't have a database setup
-require 'qa' unless defined? WITHOUT_QA
-
 module Oargun::ControlledVocabularies
   extend ActiveSupport::Autoload
   autoload :Geographic
