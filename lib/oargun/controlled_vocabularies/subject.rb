e30310086bdbec20d24503710f82131185cd8648
Justin Coyne <justin@curationexperts.com>
2015-08-06 13:45:48 -0500

    An lc_names value is a valid Subject

--- lib/oargun/controlled_vocabularies/subject.rb
+++ lib/oargun/controlled_vocabularies/subject.rb
@@ -3,6 +3,7 @@ module Oargun::ControlledVocabularies
     include Oargun::RDF::Controlled
 
     use_vocabulary :lcsh, class: Oargun::Vocabularies::LCSH
+    use_vocabulary :lcnames, class: Oargun::Vocabularies::LCNAMES
     use_vocabulary :tgm, class: Oargun::Vocabularies::TGM
     use_vocabulary :aat, class: Oargun::Vocabularies::AAT
 
